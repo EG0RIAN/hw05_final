028d2c2cf244a84e750ed76c2fd165b09b8c1c8d
Egor <e.arkhiptsev@gmail.com>
2022-11-29 01:50:02 +0300

    fix after review 2.1 (temp files fix)

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,14 +1,21 @@
+import shutil
+import tempfile
+
 from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.cache import cache
-from django.test import Client, TestCase
+from django.test import Client, TestCase, override_settings
 from django.urls import reverse
+from django.conf import settings
 
 from posts.models import Comment, Group, Post, User
 
 User = get_user_model()
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostsFormsTest(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -43,6 +50,11 @@ class PostsFormsTest(TestCase):
         self.authorized_client = Client()
         self.authorized_client.force_login(self.author)
         cache.clear()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def test_posts_forms_create_post(self):
         """Проверка, создает ли форма пост в базе."""
