2e0683346dd565a317119f9bdef6b06512a23f0e
Egor <e.arkhiptsev@gmail.com>
2022-12-01 20:18:38 +0300

    fix after review 4 (not all)

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,5 +1,8 @@
+import shutil
+import tempfile
 from http import HTTPStatus
 
+from django.conf import settings
 from django import forms
 from django.contrib.auth import get_user_model
 from django.core.cache import cache
@@ -9,10 +12,11 @@ from django.test import Client, TestCase
 from django.urls import reverse
 
 from posts.models import Comment, Follow, Group, Post
-
 from posts.utils import POST_PER_PAGE
 
 User = get_user_model()
+
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
 class PostsViewsTests(TestCase):
@@ -33,7 +37,7 @@ class PostsViewsTests(TestCase):
             content_type='image/gif'
         )
         cls.user = User.objects.create_user(username='Тестовый пользователь')
-        cls.user_another = User.objects.create_user(
+        cls.not_author = User.objects.create_user(
             username='Другой тестовый пользователь'
         )
         cls.group = Group.objects.create(
@@ -51,18 +55,23 @@ class PostsViewsTests(TestCase):
         cls.index = ('posts:index', None)
         cls.group_page = ('posts:group_list', ['test-slug'])
         cls.profile = ('posts:profile', [cls.user])
-        cls.profile_another = ('posts:profile', [cls.user_another])
+        cls.profile_another = ('posts:profile', [cls.not_author])
         cls.detail = ('posts:post_detail', [cls.post.id])
         cls.create = ('posts:create_post', None)
         cls.edit = ('posts:post_edit', [cls.post.id])
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+        cache.clear()
 
     def setUp(self):
         self.guest_client = Client()
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
-        self.authorized_client_another = Client()
-        self.authorized_client_another.force_login(self.user_another)
-        cache.clear()
+        self.authorized_client_not_author = Client()
+        self.authorized_client_not_author.force_login(self.not_author)
 
     def posts_check_all_fields(self, post):
         """Метод, проверяющий поля поста."""
@@ -73,6 +82,7 @@ class PostsViewsTests(TestCase):
 
     def test_posts_pages_use_correct_template(self):
         """Проверка, использует ли адрес URL соответствующий шаблон."""
+        cache.clear()
         templates_pages_names = (
             (
                 'posts/index.html',
@@ -118,6 +128,7 @@ class PostsViewsTests(TestCase):
         правильным контекстом.
         Появляется ли пост, при создании на странице его группы.
         """
+        cache.clear()
         template_address, argument = self.group_page
         response = self.authorized_client.get(
             reverse(template_address, args=argument)
@@ -125,12 +136,14 @@ class PostsViewsTests(TestCase):
         test_group = response.context['group']
         self.posts_check_all_fields(response.context['page_obj'][0])
         self.assertEqual(test_group, self.group)
+        cache.clear()
 
     def test_posts_context_post_create_template(self):
         """
         Проверка, сформирован ли шаблон post_create с
         правильным контекстом.
         """
+        cache.clear()
         template_address, argument = self.create
         response = self.authorized_client.get(reverse(
             template_address, args=argument)
@@ -151,6 +164,7 @@ class PostsViewsTests(TestCase):
         Проверка, сформирован ли шаблон post_edit с
         правильным контекстом.
         """
+        cache.clear()
         template_address, argument = self.edit
         response = self.authorized_client.get(
             reverse(template_address, args=argument)
@@ -172,6 +186,7 @@ class PostsViewsTests(TestCase):
         Проверка, сформирован ли шаблон profile с
         правильным контекстом.
         """
+        cache.clear()
         template_address, argument = self.profile
         response = self.authorized_client.get(
             reverse(template_address, args=argument)
@@ -186,6 +201,7 @@ class PostsViewsTests(TestCase):
         Проверка, сформирован ли шаблон post_detail с
         правильным контекстом.
         """
+        cache.clear()
         template_address, argument = self.detail
         response = self.authorized_client.get(
             reverse(template_address, args=argument)
@@ -194,6 +210,7 @@ class PostsViewsTests(TestCase):
 
     def test_post_in_author_profile(self):
         """Пост попадает в профиль к автору, который его написал."""
+        
         template_address, argument = self.profile
 
         first_object = self.authorized_client.get(reverse(
@@ -209,7 +226,7 @@ class PostsViewsTests(TestCase):
         """Пост не попадает в профиль к автору, который его не написал."""
         template_address, argument = self.profile_another
 
-        first_object = self.authorized_client_another.get(reverse(
+        first_object = self.authorized_client_not_author.get(reverse(
             template_address, args=argument
         )
         ).context['page_obj'].object_list
@@ -239,6 +256,7 @@ class PostsViewsTests(TestCase):
         комментировать посты может только
         авторизованный пользователь.
         """
+        cache.clear()
         tasks_count = Post.objects.count()
         form_data = {
             'post': self.post,
@@ -286,7 +304,7 @@ class PostsViewsTests(TestCase):
             на других пользователей."""
         followers_count = Follow.objects.count()
         one_more_follower = 1
-        response = self.authorized_client_another.post(
+        response = self.authorized_client_not_author.post(
             reverse('posts:profile_follow', args=[self.user])
         )
         self.assertRedirects(
@@ -338,6 +356,7 @@ class PostsPaginatorViewsTests(TestCase):
         """При создании пост не появляется
         в не предназначенной
         для него группе."""
+        cache.clear()
         group_two = Group.objects.create(
             title='Тестовая группа №2',
             slug='test-slug-two',
